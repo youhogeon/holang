079be333254250e6d984d89b139faa70cc5de39b
YouHoGeon <me@youhogeon.com>
2025-09-07 07:05:08 +0000

    이름 변경 (lhox -> holang)

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,20 +7,20 @@ import (
 
 func main() {
 	if len(os.Args) > 2 {
-		log.Fatal("Usage: lhox [file]", log.A("args", os.Args))
+		log.Fatal("Usage: holang [file]", log.A("args", os.Args))
 
 		return
 	}
 
 	if len(os.Args) == 2 {
 		fileName := os.Args[1]
-		log.Info("LHOX with file", log.S("file", fileName))
+		log.Info("HOLANG with file", log.S("file", fileName))
 
 		runFile(fileName)
 
 		return
 	}
 
-	log.Info("LHOX Loop Start")
+	log.Info("HOLANG Loop Start")
 	runLoop()
 }
